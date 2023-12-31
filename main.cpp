f32e9a5e0df23c2a1ec74283179af6e923002c77
Wayne Lee <waynelee@Wayne-brand-new-MacBook-Pro.local>
2023-12-31 23:30:50 +0900

    입력 에러처리를 input_check라는 함수로 따로 빼서 해결합니다

--- main.cpp
+++ main.cpp
@@ -3,15 +3,33 @@
 #include <sstream>
 #include <string>
 
+using std::cerr;
 using std::cout;
 using std::ifstream;
 using std::string;
 using std::stringstream;
 
-int main(int argc, char *argv[])
+int input_check(int argc, string argv)
 {
   if (argc != 2)
-    return -1;
+  {
+    cerr << "텍스트 파일 하나가 입력값이어야 합니다.\n";
+    return 1;
+  }
+  if (argv.compare("sudoku.txt"))
+  {
+    cerr << "텍스트 파일은 sudoku.txt 만 허용합니다.\n";
+    return 1;
+  }
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  if (input_check(argc, argv[argc - 1]))
+  {
+    return 1;
+  }
   unsigned int map[10][9];
   unsigned int y = 0;
   unsigned int x = 0;
